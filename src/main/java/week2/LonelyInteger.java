dd35291bcd0894b4fd210fc3f829476258e9f568
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-16 11:15:26 +0100

    solve LonelyInteger (find the no duplication in arraylist!)

---
+++ src/main/java/week2/LonelyInteger.java
@@ -0,0 +1,39 @@
+package week2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class LonelyInteger {
+
+    public static void main(String[] args) {
+
+        List<Integer> arrayList = new ArrayList<>();
+
+        arrayList.add(1);
+        arrayList.add(3);
+        arrayList.add(2);
+        arrayList.add(6);
+        arrayList.add(1);
+        arrayList.add(2);
+        arrayList.add(3);
+
+        int noDuplicated = lonelyInteger(arrayList);
+        System.out.println(noDuplicated);
+    }
+
+    public static int lonelyInteger(List<Integer> a) {
+        // Write your code here
+        int noDuplicatedNum = 0;
+
+        Collections.sort(a);
+        for (int i = 0; i < a.size(); i++) {
+            for (int j = i + 1; j < a.size(); j++) {
+                if (!a.get(i).equals(a.get(j))) {
+                    noDuplicatedNum = a.get(j);
+                }
+            }
+        }
+        return noDuplicatedNum;
+    }
+}
---
+++ src/main/java/week2/LonelyIntegerSolution.java
@@ -0,0 +1,30 @@
+package week2;
+
+import java.util.Collections;
+import java.util.List;
+
+public class LonelyIntegerSolution {
+
+    // working with all tests!
+
+    public static int lonelyInteger1(List<Integer> a) {
+        // Write your code here
+
+        Collections.sort(a);
+        for (int i = 0; i < a.size() - 1; i += 2) {
+            if (a.get(i) != a.get(i + 1)) return a.get(i);
+        }
+        return a.get(a.size() - 1);
+    }
+
+    public static int lonelyInteger2(List<Integer> a) {
+        // Write your code here
+
+        int unique=a.get(0);
+        for(int i=1;i<a.size();i++){
+            unique^=a.get(i);
+        }
+        return unique;
+    }
+}
+
