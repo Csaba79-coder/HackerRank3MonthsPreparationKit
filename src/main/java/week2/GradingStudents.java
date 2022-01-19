5935eb40a2768f72d3b9205c5210a865dcde53b3
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-19 11:32:16 +0100

    solve GradingStudents

---
+++ src/main/java/week2/GradingStudents.java
@@ -0,0 +1,37 @@
+package week2;
+
+import java.util.ArrayList;
+import java.util.List;
+
+public class GradingStudents {
+
+    public static void main(String[] args) {
+
+        // expected output: 75, 67, 40, 33;
+
+        List<Integer> grades = new ArrayList<>();
+        grades.add(73);
+        grades.add(67);
+        grades.add(38);
+        grades.add(33);
+
+        List<Integer> list = gradingStudents(grades);
+        System.out.println(list.toString());
+    }
+
+    public static List<Integer> gradingStudents(List<Integer> grades) {
+        // Write your code here
+        ArrayList<Integer> list = new ArrayList<Integer>(grades.size());
+        for(int i: grades){
+            int diff = i % 5;
+            if(i < 38 || diff < 3){
+                list.add(i);
+            }
+            else{
+                int result = i + (5 - diff);
+                list.add(result);
+            }
+        }
+        return list;
+    }
+}
