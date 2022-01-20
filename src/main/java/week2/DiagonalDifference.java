2c27ecbaf9fd4df3f0a7ccf9304e89c18be967cb
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-20 19:49:55 +0100

    solve DiagonalDifference

---
+++ src/main/java/week2/DiagonalDifference.java
@@ -0,0 +1,18 @@
+package week2;
+
+import java.util.List;
+
+public class DiagonalDifference {
+
+    public static int diagonalDifference(List<List<Integer>> arr) {
+        // Write your code here
+        int left=0,right=0;
+        int j=arr.size();
+        for(int i=0;i<arr.size();i++){
+            j-=1;
+            left+=arr.get(i).get(i);
+            right+=arr.get(i).get(j);
+        }
+        return left>right ? left-right :right-left;
+    }
+}
