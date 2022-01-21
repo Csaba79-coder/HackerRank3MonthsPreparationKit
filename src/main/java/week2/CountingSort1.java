c7e9db3daaee882184acbdd5a66745419ad1923e
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-21 20:52:12 +0100

    solve CountingSort1

---
+++ src/main/java/week2/CountingSort1.java
@@ -0,0 +1,16 @@
+package week2;
+
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.List;
+
+public class CountingSort1 {
+
+    public static List<Integer> countingSort(List<Integer> arr) {
+        // Write your code here
+        List<Integer> res = new ArrayList<>(Collections.nCopies(100, 0));
+
+        arr.forEach((x)-> res.set(x, res.get(x) + 1));
+        return res;
+    }
+}
