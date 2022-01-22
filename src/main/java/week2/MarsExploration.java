62f2b4f5775a68aa0888ee559d9492a8b821c53b
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-22 12:02:29 +0100

    solve Mars Exploration

---
+++ src/main/java/week2/MarsExploration.java
@@ -0,0 +1,31 @@
+package week2;
+
+public class MarsExploration {
+
+    public static void main(String[] args) {
+        String s1 = "SOSSPSSQSSOR"; // output: 3
+
+        int result1 = marsExploration(s1);
+        System.out.println(result1);
+    }
+
+    public static int marsExploration(String s) {
+        // Write your code here
+        int count = 0;
+        int currentPos = 0;
+        for(char letter : s.toCharArray())
+        {
+
+            if(currentPos % 3 == 1)
+            {
+                count += (letter != 'O') ? 1 : 0;
+            }
+            else
+            {
+                count += (letter != 'S') ? 1 : 0;
+            }
+            currentPos++;
+        }
+        return count;
+    }
+}
