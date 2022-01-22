6c7f26985e0c45f0683e8b398557a117d573be3c
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-01-22 11:26:09 +0100

    solve CountingValleys

---
+++ src/main/java/week2/CountingValleys.java
@@ -0,0 +1,37 @@
+package week2;
+
+public class CountingValleys {
+
+    public static int countingValleys(int steps, String path) {
+        // Write your code here
+
+        int sea = 0;
+        int counter = 0;
+        boolean above = false;
+        boolean below = false;
+
+        for(int i = 0;i< steps;i++){
+            if(path.charAt(i)=='D'){
+                sea--;
+            }else sea++;
+            if(sea > 0) {
+                above = true;
+                if(below){
+                    below = false;
+                    counter ++;
+                }
+            }
+            if(sea < 0){
+                below = true;
+                if(above){
+                    above = false;
+                }
+            }
+            if(sea == 0 && below){
+                below = false;
+                counter++;
+            }
+        }
+        return counter;
+    }
+}
