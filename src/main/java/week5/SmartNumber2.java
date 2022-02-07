f858891973b9253c65b0de60915714f8e1de1a1b
Csaba Vadász <77699103+Csaba79-coder@users.noreply.github.com>
2022-02-07 14:22:48 +0100

    solve SmartNumber2

--- src/main/java/week5/SmartNumber2.java
+++ src/main/java/week5/SmartNumber2.java
@@ -1,4 +1,27 @@
 package week5;
 
+import java.util.Scanner;
+
 public class SmartNumber2 {
+
+    public static boolean isSmartNumber(int num) {
+        int val = (int) Math.sqrt(num);
+        if ((val * val) % num == 0)
+            return true;
+        return false;
+    }
+
+    public static void main(String[] args) {
+        int test_cases;
+        Scanner in = new Scanner(System.in);
+        test_cases = in.nextInt();
+        int num;
+        for (int i = 0; i < test_cases; i++) {
+            num = in.nextInt();
+            boolean ans = isSmartNumber(num);
+            if (ans) {
+                System.out.println("YES");
+            } else System.out.println("NO");
+        }
+    }
 }
